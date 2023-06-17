9595e4cb4c9c9c98f3e97557c633eda44a48503d
Kyle <kylepeppersack@hotmail.com>
2023-06-17 10:35:21 -0600

    Done?

--- app.js
+++ app.js
@@ -12,33 +12,40 @@ const up33 = document.getElementById('upgrade33')
 const up4 = document.getElementById('upgrade4')
 const up44 = document.getElementById('upgrade44')
 
-console.log(document.getElementById('upgrade11'))
+console.log(document.getElementById('upgrade1'))
+console.log(document.getElementById('upgrade2'))
+console.log(document.getElementById('upgrade3'))
+console.log(document.getElementById('upgrade4'))
 
 let upgrades = [
   {
     name: 'upgrade1',
-    cost: 1,
+    cost: 50,
+    costMuliplier: 1.25,
     value: 0,
     mulitplier: 1,
     quantity: 0,
     type: 'click'
   }, {
     name: 'upgrade2',
-    cost: 1,
+    cost: 300,
+    costMuliplier: 1.25,
     value: 0,
     mulitplier: 5,
     quantity: 0,
     type: 'click'
   }, {
     name: 'upgrade3',
-    cost: 1,
+    cost: 250,
+    costMuliplier: 1.25,
     value: 0,
     quantity: 0,
     mulitplier: 1,
     type: 'interval'
   }, {
     name: 'upgrade4',
-    cost: 1,
+    cost: 1000,
+    costMuliplier: 1.25,
     value: 0,
     quantity: 0,
     mulitplier: 5,
@@ -65,23 +72,23 @@ function drawresource() {
 }
 function drawUpgrade() {
   // @ts-ignore
+  //up1.querySelector("img").innerText = `+ 1 click`
   up1.querySelector("p").innerText = `cost: ${upgrades[0].cost}`
-  up1.querySelector("img").innerText = `+ 1 click`
   // @ts-ignore
   up11.querySelector("span").innerText = `: ${upgrades[0].quantity}`
   // @ts-ignore
+  //up2.querySelector("img").innerText = `+ 5 click`
   up2.querySelector("p").innerText = `cost: ${upgrades[1].cost}`
-  up2.querySelector("img").innerText = `+ 5 click`
   // @ts-ignore
   up22.querySelector("span").innerText = `: ${upgrades[1].quantity}`
   // @ts-ignore
+  //up3.querySelector("img").innerText = `+ 5 auto`
   up3.querySelector("p").innerText = `cost: ${upgrades[2].cost}`
-  up3.querySelector("img").innerText = `+ 5 auto`
   // @ts-ignore
   up33.querySelector("span").innerText = `: ${upgrades[2].quantity}`
   // @ts-ignore
+  //up4.querySelector("img").innerText = `+ 5 auto`
   up4.querySelector("p").innerText = `cost: ${upgrades[3].cost}`
-  up4.querySelector("img").innerText = `+ 5 auto`
   // @ts-ignore
   up44.querySelector("span").innerText = `: ${upgrades[3].quantity}`
 }
@@ -89,7 +96,7 @@ function buyUpgrade(targetUpgrade) {
   let purchaseUpgrade = upgrades.find(upgrade => upgrade.name == targetUpgrade)
   if (resource >= purchaseUpgrade.cost) {
     resource -= purchaseUpgrade.cost;
-    purchaseUpgrade.cost++;
+    purchaseUpgrade.cost = Math.floor(purchaseUpgrade.cost * purchaseUpgrade.costMuliplier)
     purchaseUpgrade.quantity++;
     purchaseUpgrade.value += (1 * purchaseUpgrade.mulitplier);
     statsUpdate()
