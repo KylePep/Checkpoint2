500473425978af02277b552b8985381d3fb03e33
Kyle <kylepeppersack@hotmail.com>
2023-06-17 22:54:42 -0600

    Highscore mostly

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ let musicUnlock = false;
 let lights = false;
 let achievementCurrent = 0;
 let achievementTotal = 11;
+let interval = 3000;
+
 
 const up1 = document.getElementById('upgrade1')
 const up11 = document.getElementById('upgrade11')
@@ -20,6 +22,13 @@ console.log(document.getElementById('upgrade1'))
 console.log(document.getElementById('upgrade2'))
 console.log(document.getElementById('upgrade3'))
 console.log(document.getElementById('upgrade4'))
+
+let highScoreBonus = {
+  bonusActive: false,
+  bonusModifier: 2,
+  bonusRounds: 0,
+  bonusRoundsMax: 10,
+}
 
 let upgrades = [
 
@@ -47,7 +56,7 @@ let upgrades = [
     costMuliplier: 1.25,
     value: 0,
     quantity: 0,
-    mulitplier: 1,
+    mulitplier: 5,
     type: 'interval',
     unlocked: false
   }, {
@@ -56,7 +65,7 @@ let upgrades = [
     costMuliplier: 1.25,
     value: 0,
     quantity: 0,
-    mulitplier: 10,
+    mulitplier: 50,
     type: 'interval',
     unlocked: false
   }
@@ -64,67 +73,67 @@ let upgrades = [
 
 let achievements = [
   {
-    name: '10 responsibilites avoided',
+    name: 'Eveyones asleep, Maybe we should play?',
     Type: 'resource',
     arrayPosition: 0,
     condition: 10,
     locked: true,
   }, {
-    name: 'Buy Book',
+    name: 'Avoided homework',
     Type: 'mile stone',
     arrayPosition: 0,
     condition: 1,
     locked: true,
   }, {
-    name: 'Buy Game',
+    name: 'New Game just came out!',
     Type: 'mile stone',
     arrayPosition: 1,
     condition: 1,
     locked: true,
   }, {
-    name: 'Buy Snack',
+    name: 'Heck yeah, Chips!',
     Type: 'mile stone',
     arrayPosition: 2,
     condition: 1,
     locked: true,
   }, {
-    name: 'Buy Left Over',
+    name: 'This still looks good',
     Type: 'mile stone',
     arrayPosition: 3,
     condition: 1,
     locked: true,
   }, {
-    name: 'Buy 5 Books',
+    name: "I'll just pull another all nighter",
     Type: 'mile stone',
     arrayPosition: 0,
     condition: 5,
     locked: true,
   }, {
-    name: 'Buy 3 Games',
+    name: 'Working on that back log',
     Type: 'mile stone',
     arrayPosition: 1,
     condition: 3,
     locked: true,
   }, {
-    name: 'Buy 4 Snacks',
+    name: "What're these? cashews?",
     Type: 'mile stone',
     arrayPosition: 2,
     condition: 4,
     locked: true,
   }, {
-    name: 'Buy 2 Left Overs',
+    name: 'How much mold is too much mold?',
     Type: 'mile stone',
     arrayPosition: 3,
     condition: 2,
     locked: true,
   }, {
-    name: '500 responsibilites avoided',
+    name: 'You found your headphones, plug in and listen?',
     Type: 'resource',
     arrayPosition: 0,
     condition: 500,
     locked: true,
   }, {
-    name: '10,000 responsibilites avoided',
+    name: '10,000 responsibilites avoided, and its 3 AM ...',
     Type: 'resource',
     arrayPosition: 0,
     condition: 10000,
@@ -135,9 +144,14 @@ let achievements = [
 
 
 function resoureClick() {
-  resource += resourcePerClick
-  resourceCareer += resourcePerClick
-  console.log(resource)
+  if (highScoreBonus.bonusActive == false) {
+    resource += resourcePerClick
+    resourceCareer += resourcePerClick
+  } else {
+    resource += resourcePerClick
+    resourceCareer += resourcePerClick
+  }
+
   checkUnlock()
   drawresource()
 }
@@ -146,11 +160,13 @@ function drawresource() {
   // @ts-ignore
   document.getElementById('resourceQty').innerText = `Responsibilities avoided: ${resource}`
   let resourceTotal = document.getElementById('resourceCareer')
+  // @ts-ignore
   resourceTotal.innerText = `Tomorrow me's Problems: ${resourceCareer}`
   // @ts-ignore
   document.getElementById('resourceInterval').innerText = `${resourcePerInterval}`
   // @ts-ignore
   document.getElementById('resourceClick').innerText = `${resourcePerClick}`
+  // @ts-ignore
   document.getElementById('achievement').innerText = `Achievement: ${achievementCurrent}/${achievementTotal}`
 }
 function drawUpgrade() {
@@ -185,9 +201,6 @@ function buyUpgrade(targetUpgrade) {
     statsUpdate()
     checkUnlock()
   }
-
-
-
   drawUpgrade()
   drawresource()
 }
@@ -202,29 +215,74 @@ function statsUpdate() {
       perTemp += upgrade.value
     }
   })
-  resourcePerClick = (1 + perTemp)
-  resourcePerInterval = intervalTemp
+  if (highScoreBonus.bonusActive == true) {
+    resourcePerClick = (1 + perTemp) * highScoreBonus.bonusModifier
+    resourcePerInterval = intervalTemp * highScoreBonus.bonusModifier
+  } else {
+    resourcePerClick = (1 + perTemp)
+    resourcePerInterval = intervalTemp
+  }
 }
 
 
 function intervalUpgrade() {
   upgrades.forEach(upgrade => {
     if (upgrade.type == 'interval') {
-      resource += upgrade.value
-      resourceCareer += upgrade.value
-      drawresource()
+      if (highScoreBonus.bonusActive == false) {
+        resource += upgrade.value
+        resourceCareer += upgrade.value
+      } else {
+        resource += (upgrade.value * highScoreBonus.bonusModifier)
+        resourceCareer += (upgrade.value * highScoreBonus.bonusModifier)
+      }
     }
   })
+  if (highScoreBonus.bonusRounds > 0) {
+    highScoreBonus.bonusRounds--
+    console.log(highScoreBonus.bonusRounds)
+  } else {
+    highScoreBonus.bonusActive = false;
+  }
+  let i = Math.random()
+  //console.log(i, achievementCurrent, highScoreBonus.bonusRounds)
+  if (i >= .9 && achievementCurrent >= 3 && highScoreBonus.bonusActive == false) {
+    document.getElementById('score').innerHTML += `
+    <marquee id="highscore" class="highscore" direction="down" behavior="alternate" Scrollamount=14 style="border:empty">
+    <marquee behavior="alternate" Scrollamount=14>
+      <Div class="fs-1" onclick="clickHighScore()">!HIGHSCORE!</Div>
+    </marquee>
+  </marquee>
+    `
+  }
+
+  if (highScoreBonus.bonusActive == true) {
+    let barPercent = (highScoreBonus.bonusRounds / highScoreBonus.bonusRoundsMax) * 100
+
+    document.getElementById('progressBar').innerHTML = `
+      <div id="highScoreBar" class="progress" role="progressbar">
+      <div class="progress-bar scoreBar" style="width: ${barPercent}%"></div>
+    </div>
+      `
+  } else {
+    if (document.getElementById('highScoreBar') != null) {
+      document.getElementById('highScoreBar').remove()
+    }
+  }
+
+  statsUpdate()
+  drawresource()
 }
 
 function checkUnlock() {
   if (resource >= 500 && musicUnlock == false) {
-    window.alert("You found your headphones plug in and listen?")
+    //window.alert("You found your headphones plug in and listen?")
+    // @ts-ignore
     document.getElementById('music').querySelector('audio').classList.remove('hidden')
     musicUnlock = true;
   }
   if (resource >= 10 && lights == false) {
-    window.alert("Eveyones asleep, Maybe we should play?")
+    //window.alert("Eveyones asleep, Maybe we should play?")
+    // @ts-ignore
     document.getElementById('body').classList.replace('bg-hidden', 'bg')
     lights = true;
   }
@@ -234,7 +292,8 @@ function checkUnlock() {
 
   resourceAchievements.forEach(achievement => {
     if ((resource >= achievement.condition) && achievement.locked == true) {
-      window.alert(`Achievement: ${achievement.name}`)
+      //window.alert(`Achievement: ${achievement.name}`)
+      achievementMessage(achievement)
       achievement.locked = false
       achievementCurrent++
       console.log(achievement)
@@ -243,7 +302,8 @@ function checkUnlock() {
 
   mileStoneAchievement.forEach(achievement => {
     if ((upgrades[achievement.arrayPosition].quantity >= achievement.condition) && achievement.locked == true) {
-      window.alert(`Achievement: ${achievement.name}`)
+      //window.alert(`Achievement: ${achievement.name}`)
+      achievementMessage(achievement)
       achievement.locked = false
       achievementCurrent++
       console.log(achievement)
@@ -256,17 +316,51 @@ function checkUnlock() {
     }
   })
   if (upgrades[1].unlocked == true) {
+    // @ts-ignore
     up2.querySelector('img').classList.replace('locked', 'unlocked')
   }
   if (upgrades[2].unlocked == true) {
+    // @ts-ignore
     up3.querySelector('img').classList.replace('locked', 'unlocked')
   }
   if (upgrades[3].unlocked == true) {
+    // @ts-ignore
     up4.querySelector('img').classList.replace('locked', 'unlocked')
   }
   drawresource()
 }
 
+function achievementMessage(achievement) {
+  // @ts-ignore
+  Swal.fire({
+    position: 'top-end',
+    color: '#14ff0c',
+    background: '#313431',
+    title: `Achievement: ${achievement.name}`,
+    showConfirmButton: false,
+    timer: 2000
+  })
+  // @ts-ignore
+  document.getElementById('body').classList.add('stop-click')
+}
+function removeStopClick() {
+  // @ts-ignore
+  if (document.getElementById('body').classList.contains('stop-click'))
+    // @ts-ignore
+    document.getElementById('body').classList.remove('stop-click')
+}
+
+function clickHighScore() {
+  let highscore = document.getElementById('highscore');
+  // @ts-ignore
+  highscore.remove();
+
+  highScoreBonus.bonusRounds = highScoreBonus.bonusRoundsMax;
+  highScoreBonus.bonusActive = true;
+  statsUpdate()
+}
+
 drawUpgrade()
 drawresource()
-setInterval(intervalUpgrade, 3000)
+setInterval(removeStopClick, 3000)
+setInterval(intervalUpgrade, interval)
